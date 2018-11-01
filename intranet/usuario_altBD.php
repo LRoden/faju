fe428aa86218b22874b7c30676014254dd4f2011
Leonardo Roden <leonardoroden.ti@gmail.com>
2018-11-01 16:45:43 -0200

    faju
    Att-senha md5

--- intranet/usuario_altBD.php
+++ intranet/usuario_altBD.php
@@ -7,6 +7,7 @@ $usuario_nome = $_POST ['usuario_nome'];
 $usuario_login = $_POST ['usuario_login'];
 $usuario_email = $_POST['usuario_email'];
 $usuario_senha =$_POST ['usuario_senha'];
+$usuario_senha =md5($usuario_senha);
 
 $id =$_GET['id'];
 //enviar dados
