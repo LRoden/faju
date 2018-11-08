51e94b4cbebe39b135fb820b984530de71cdf36a
Sibele Mueller <sibele@uceff.edu.com>
2018-11-08 16:58:41 -0200

    roteiro

--- roteiro3.php
+++ roteiro3.php
@@ -1,6 +1,17 @@
+<?php
+$roteiro_id = $_GET["url"];
+//echo $roteiro_id;
+$id = str_replace("rot", "", $roteiro_id);
+
+include './conexao.php';
+$sqli = "select * "
+        . "from roteiro_lugares, parada, lugares where roteiro_lugares.lugares_id = lugares.id and roteiro_lugares.parada_id = parada.id and roteiro_lugares.roteiro_id = $id;";
+$query = $mysqli->query($sqli);
+//print_r($query);
+?>
+
 <!DOCTYPE html>
 <html lang="en">
-
     <head>
         <link href="css/agency.min.css" rel="stylesheet">
         <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
@@ -9,140 +20,93 @@
         <link href="https://fonts.googleapis.com/css?family=Lora:400,400i,700,700i" rel="stylesheet">
         <!-- Custom styles for this template -->
         <link href="css/business-casual.min.css" rel="stylesheet">
-
     </head>
 
 
     <body>
-
         <!-- About -->
-        <sectionrot id="about">
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-12 text-center">
-                        <h4 class="site-heading text-center text-white d-none d-lg-block">
-                            <span>ROTEIRO PÔR DO SOL</span>
-                        </h4>
-                    </div>
-
+    <sectionrot id="about">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h4 class="site-heading text-center text-white d-none d-lg-block">
+                        <span>ROTEIRO PÔR DO SOL</span>
+                    </h4>
                 </div>
-                <div class="row">
-                    <div class="col-lg-12">
-                        <ul class="timeline">
-                            <li>
-                                <div class="timeline-image">
-                                    <img class="rounded-circle img-fluid" src="img/about/1.jpg" alt="">
-                                </div>
-                                <div class="timeline-panel">                           
-                                    <div class="intro-text left-0 bg-faded p-3 rounded">    
 
-                                        <div class="timeline-heading">
-                                            <h4>Início</h4>
-                                            <h4 class="subheading">Saída Praça dos Imigrantes (figueira)</h4>
-                                        </div>
-                                        <div class="timeline-body">
-                                            <p class="text-muted">Neste local, estão edificados alguns monumentos que representam a história de Itapiranga-SC, sendo realizada a primeira missa rezada na cidade no ano de 1926. No monumento, estão os nomes dos pioneiros, uma máquina à vapor, a muda da figueira da praça 15 de Florianópolis-SC.
-O Centro Empresarial e a Associação dos Artesão de Itapiranga-SC está localizado junto às exposições históricas e a rodoviária.</p>
+                <?php
+                $count = 0;
+                foreach ($query as $q):
+                    $count = $count + 1;
+                endforeach;
+                ?>
+            </div>
+            <div class="row">
+                <div class="col-lg-12">
+                    <ul class="timeline">
+
+                        <?php
+                        $count2 = 0;
+                        foreach ($query as $q):
+                            $count2 = $count2 + 1;
+                            if ($count2 % 2 != 0) {
+                                //Par
+                                ?>
+                                <li>
+                                    <div class="timeline-image">
+                                        <img class="rounded-circle img-fluid" src="upload/lugar/<?php $q['foto_principal'] ?>" alt="">
+                                    </div>
+                                    <div class="timeline-panel">                           
+                                        <div class="intro-text left-0 bg-faded p-3 rounded">    
+
+                                            <div class="timeline-heading">
+
+                                                <h4 class="subheading"><?php echo $q['nome'] ?></h4>
+                                            </div>
+                                            <div class="timeline-body">
+                                                <p class="text-muted"><?php echo $q['descricao'] ?></p>
+                                            </div>
                                         </div>
                                     </div>
-                                </div>
+                                </li>
+
+                                <?php
+                            } else {
+                                //Impar;
+                                ?>
+                                <li class="timeline-inverted">
+                                    <div class="timeline-image">
+                                        <img class="rounded-circle img-fluid" src="upload/lugar/<?php $q['foto_principal'] ?>" alt="">
+                                    </div>
+                                    <div class="timeline-panel">
+                                        <div class="intro-text left-0 bg-faded p-3 rounded">    
+                                            <div class="timeline-heading">
+
+                                                <h4 class="subheading"><?php echo $q['nome'] ?></h4>
+                                            </div>
+                                            <div class="timeline-body">
+                                                <p class="text-muted"><?php echo $q['descricao'] ?></p>
+                                            </div>
+                                        </div>    
+                                    </div>
+                                </li>
+
+                            <?php } ?>
+
+                        <?php endforeach; ?>
 
 
-                            </li>
-                            <li class="timeline-inverted">
-                                <div class="timeline-image">
-                                    <img class="rounded-circle img-fluid" src="img/about/2.jpg" alt="">
-                                </div>
-                                <div class="timeline-panel">
-                                    <div class="intro-text left-0 bg-faded p-3 rounded">    
-                                        <div class="timeline-heading">
-                                            <h4></h4>
-                                            <h4 class="subheading"> Igreja e Santuário em Linha Popi</h4>
-                                        </div>
-                                        <div class="timeline-body">
-                                            <p class="text-muted">Esta é a única Igreja em madeira, com duas torres, edificada em enxaimel, revestida com tábuas, da América Latina.</p>
-                                        </div>
-                                    </div>    
-                                </div>
-                            </li>
-                            <li>
-                                <div class="timeline-image">
-                                    <img class="rounded-circle img-fluid" src="img/about/3.jpg" alt="">
-                                </div>
-                                <div class="timeline-panel">
-                                    <div class="intro-text left-0 bg-faded p-3 rounded">  
-                                        <div class="timeline-heading">
-                                            <h4> </h4>
-                                            <h4 class="subheading"> Arte em Pedras Kummer (La. Popi): </h4>
-                                        </div>
-                                        <div class="timeline-body">
-                                            <p class="text-muted">Trabalho artesanal em pedras naturais, à venda aos turistas.</p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </li>
-                            <li class="timeline-inverted">
-                                <div class="timeline-image">
-                                    <img class="rounded-circle img-fluid" src="img/about/4.jpg" alt="">
-                                </div>
-                                <div class="timeline-panel">
-                                    <div class="intro-text left-0 bg-faded p-3 rounded">
-                                        <div class="timeline-heading">
-                                            <h4></h4>
-                                            <h4 class="subheading"> Viveiro de Pássaros Reis (La. Soledade)</h4>
-                                        </div>
-                                        <div class="timeline-body">
-                                            <p class="text-muted">Possui variedade de mais de 40 espécies de pássaros com diversos tamanhos, para exposição e venda.</p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </li>
-                            
-                            <li>
-                                <div class="timeline-image">
-                                    <img class="rounded-circle img-fluid" src="img/about/3.jpg" alt="">
-                                </div>
-                                <div class="timeline-panel">
-                                    <div class="intro-text left-0 bg-faded p-3 rounded">  
-                                        <div class="timeline-heading">
-                                            <h4> </h4>
-                                            <h4 class="subheading"> Propriedade Stoffel - Trilha e Almoço (La. Sta. Isabel) </h4>
-                                        </div>
-                                        <div class="timeline-body">
-                                            <p class="text-muted">Área de lazer com trilha para apreciação de  árvores antigas. São servidas refeições, como almoço, janta ou lanche.</p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </li>
-                            <li class="timeline-inverted">
-                                <div class="timeline-image">
-                                    <img class="rounded-circle img-fluid" src="img/about/4.jpg" alt="">
-                                </div>
-                                <div class="timeline-panel">
-                                    <div class="intro-text left-0 bg-faded p-3 rounded">
-                                        <div class="timeline-heading">
-                                            <h4></h4>
-                                            <h4 class="subheading"> Recanto Goergen - Peixe Frito ou Café Colonial (La. Popi)</h4>
-                                        </div>
-                                        <div class="timeline-body">
-                                            <p class="text-muted">Área de lazer para descanso em meio à natureza, com excelente espaço para diversão e cabanas para estadia noturna.</p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </li>
-                            <li class="timeline-inverted">
-                                <div class="timeline-image">
-                                    <h4> Retorno 
-                                        <br>até a figueira -
-                                        <br> Praça!</h4>
-                                </div>
-                            </li>
-                        </ul>
-                    </div>
+
+
+
+
+
+                    </ul>
                 </div>
             </div>
-        </sectionrot> 
+        </div>
+    </sectionrot> 
 
-    </body>
+</body>
 
 </html>
