62d01165e614c964d1066f42c50f1cd1e8bbced9
Sibele Mueller <sibele@uceff.edu.com>
2018-12-06 15:05:32 -0200

    mostra imagem

--- roteiro.php
+++ roteiro.php
@@ -11,108 +11,108 @@ $query = $mysqli->query($sqli);
 //print_r($query);
 ?>
 
-    <sectionrot id="about">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12 text-center">
-                    <h4 class="site-heading text-center text-white d-none d-lg-block">
-                        <span>ROTEIRO PÔR DO SOL</span>
-                    </h4>
-                </div>
-
-                <?php
-                $count = 0;
-                foreach ($query as $q):
-                    $count = $count + 1;
-                endforeach;
-                ?>
+<sectionrot id="about">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 text-center">
+                <h4 class="site-heading text-center text-white d-none d-lg-block">
+                    <span>ROTEIRO PÔR DO SOL</span>
+                </h4>
             </div>
-            <div class="row">
-                <div class="col-lg-12">
-                    <ul class="timeline">
 
-                        <?php
-                        $count2 = 0;
-                        foreach ($query as $q):
-                            $count2 = $count2 + 1;
-                            if ($count2 % 2 != 0) {
-                                //Par
-                                ?>
-                                <li class="timeline-image">
-                                    <div class="timeline-image">
-                                        <img class="rounded-circle img-fluid" src="upload/lugar/<?php $q['foto_principal'] ?>" alt="">
-                                    </div>
-                                    <div class="timeline-panel">                           
-                                        <div class="intro-text left-0 bg-faded p-3 rounded">    
+            <?php
+            $count = 0;
+            foreach ($query as $q):
+                $count = $count + 1;
+            endforeach;
+            ?>
+        </div>
+        <div class="row">
+            <div class="col-lg-12">
+                <ul class="timeline">
 
-                                            <div class="timeline-heading">
+                    <?php
+                    $count2 = 0;
+                    foreach ($query as $q):
+                        $count2 = $count2 + 1;
+                        if ($count2 % 2 != 0) {
+                            //Par
+                            ?>
+                            <li class="timeline-image">
+                                <div class="timeline-image">
+                                    <img class="rounded-circle img-fluid" src="upload/lugar/<?php echo $q['foto_principal'] ?>" alt="">
+                                </div>
+                                <div class="timeline-panel">                           
+                                    <div class="intro-text left-0 bg-faded p-3 rounded">    
 
-                                                <h4 class="subheading"><?php echo $q['nome'] ?></h4>
-                                            </div>
-                                            <div class="timeline-body">
-                                                <p class="text-muted"><?php echo $q['descricao'] ?></p>
-                                            </div>
+                                        <div class="timeline-heading">
+
+                                            <h4 class="subheading"><?php echo $q['nome'] ?></h4>
+                                        </div>
+                                        <div class="timeline-body">
+                                            <p class="text-muted"><?php echo $q['descricao'] ?></p>
                                         </div>
                                     </div>
-                                    
-                                    
-<!--                                     <li class="timeline-inverted"> 
-                                <div class="timeline-image"> 
-                                    <img class="rounded-circle img-fluid" src="img/about/2.jpg" alt=""> 
-                                </div> 
-                                <div class="timeline-panel"> 
-                                    <div class="intro-text left-0 bg-faded p-3 rounded">     
-                                        <div class="timeline-heading"> 
-                                            <h4></h4> 
-                                            <h4 class="subheading"> Igreja e Santuário em Linha Popi</h4> 
-                                        </div> 
-                                        <div class="timeline-body"> 
-                                            <p class="text-muted">Esta é a única Igreja em madeira, com duas torres, edificada em enxaimel, revestida com tábuas, da América Latina.</p> 
-                                        </div> 
-                                    </div>     
-                                </div> 
-                            </li> -->
-                            
-                            
-                            
-                                </li>
-                                
-                                
-                                
+                                </div>
 
-                                <?php
-                            } else {
-                                //Impar;
-                                ?>
-                                <li class="timeline-inverted">
-                                    <div class="timeline-image">
-                                        <img class="rounded-circle img-fluid" src="upload/lugar/<?php $q['foto_principal'] ?>" alt="">
-                                    </div>
-                                    <div class="timeline-panel">
-                                        <div class="intro-text left-0 bg-faded p-3 rounded">    
-                                            <div class="timeline-heading">
 
-                                                <h4 class="subheading"><?php echo $q['nome'] ?></h4>
-                                            </div>
-                                            <div class="timeline-body">
-                                                <p class="text-muted"><?php echo $q['descricao'] ?></p>
-                                            </div>
-                                        </div>    
-                                    </div>
-                                </li>
+                                <!--                                     <li class="timeline-inverted"> 
+                                                                <div class="timeline-image"> 
+                                                                    <img class="rounded-circle img-fluid" src="img/about/2.jpg" alt=""> 
+                                                                </div> 
+                                                                <div class="timeline-panel"> 
+                                                                    <div class="intro-text left-0 bg-faded p-3 rounded">     
+                                                                        <div class="timeline-heading"> 
+                                                                            <h4></h4> 
+                                                                            <h4 class="subheading"> Igreja e Santuário em Linha Popi</h4> 
+                                                                        </div> 
+                                                                        <div class="timeline-body"> 
+                                                                            <p class="text-muted">Esta é a única Igreja em madeira, com duas torres, edificada em enxaimel, revestida com tábuas, da América Latina.</p> 
+                                                                        </div> 
+                                                                    </div>     
+                                                                </div> 
+                                                            </li> -->
 
-                            <?php } ?>
 
-                        <?php endforeach; ?>
+
+                            </li>
 
 
 
 
+                            <?php
+                        } else {
+                            //Impar;
+                            ?>
+                            <li class="timeline-inverted">
+                                <div class="timeline-image">
+                                    <img class="rounded-circle img-fluid" src="upload/lugar/<?php echo $q['foto_principal'] ?>" width="200px" height="200px" alt="">
+                                </div>
+                                <div class="timeline-panel">
+                                    <div class="intro-text left-0 bg-faded p-3 rounded">    
+                                        <div class="timeline-heading">
+
+                                            <h4 class="subheading"><?php echo $q['nome'] ?></h4>
+                                        </div>
+                                        <div class="timeline-body">
+                                            <p class="text-muted"><?php echo $q['descricao'] ?></p>
+                                        </div>
+                                    </div>    
+                                </div>
+                            </li>
+
+                        <?php } ?>
+
+                    <?php endforeach; ?>
 
 
 
-                    </ul>
-                </div>
+
+
+
+
+                </ul>
             </div>
         </div>
-    </sectionrot> 
+    </div>
+</sectionrot> 
